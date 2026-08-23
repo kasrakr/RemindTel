f2b782b69232fec772812518525ca86267f9b432
Kasra <kasrakarimaian84@gmail.com>
2026-08-23 12:12:10 +0330

    creating database

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy import BIGINT, VARCHAR
+from datetime import datetime
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__='users'
+
+    id : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id : Mapped[int] = mapped_column(BIGINT(), unique=True)
+    full_name = Mapped[str] = mapped_column(VARCHAR(128), nullable=True)
+    join_date = Mapped[datetime] = mapped_column(default=datetime.now)
